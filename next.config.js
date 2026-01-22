3e1be794d28a78f9005912020b9dbdaae77dda6f
KnarliX <KnarliX@users.noreply.github.com>
2026-01-22 07:01:21 +0000

    packages update

--- next.config.js
+++ next.config.js
@@ -1,13 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // Disable type checking during build (not needed for API-only project)
   typescript: {
     ignoreBuildErrors: true,
   },
-  // Disable ESLint during build
-  eslint: {
-    ignoreDuringBuilds: true,
-  }
-}
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
